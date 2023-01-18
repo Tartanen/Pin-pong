4f0edb90849c3d697fed5d1cc2937626697c55c6
Tartanen <eidzen@mail.ru>
2023-01-18 16:35:04 +0300

    Избавил код от однотипных классов, убраты тестовые функции

--- Function.py
+++ Function.py
@@ -65,10 +65,6 @@ def terminate():
 
 
 def load_image(name1, name2):
-    """
-    Да, самый первобытный, но по факту почему-то альфа не работат
-    (из-за изображений)
-    """
     fullname = os.path.join('data', 'image', name1, name2)
     # если файл не существует, то выходим
     if not os.path.isfile(fullname):
